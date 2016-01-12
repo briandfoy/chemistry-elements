b19509295de8e4fc1bb63569bb119b9dbe1d631a
brian d foy <brian.d.foy@gmail.com>
2016-01-12 14:26:52 -0500

    Bump copyright and version

--- lib/Chemistry/Elements.pm
+++ lib/Chemistry/Elements.pm
@@ -16,7 +16,7 @@ use Exporter qw(import);
 
 @EXPORT_OK = qw(get_Z get_symbol get_name);
 @EXPORT    = qw();
-$VERSION   = '1.06';
+$VERSION   = '1.061';
 
 use subs qw(
 	_get_name_by_Z
@@ -803,7 +803,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2000-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2000-2016, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
