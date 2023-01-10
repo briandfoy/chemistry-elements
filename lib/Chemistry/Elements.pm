e6839d556afb953bdaf9bd5ec92156ebe91f3ef2
brian d foy <briandfoy@pobox.com>
2023-01-10 09:59:12 -0500

    Bump version

--- lib/Chemistry/Elements.pm
+++ lib/Chemistry/Elements.pm
@@ -19,7 +19,7 @@ use Exporter qw(import);
 
 our @EXPORT_OK = qw(get_Z get_symbol get_name);
 our @EXPORT    = qw();
-our $VERSION   = '1.075';
+our $VERSION   = '1.076';
 
 use subs qw(
 	_get_name_by_Z
